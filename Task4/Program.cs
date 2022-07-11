e963e2ae79528b758fc737c89e81e9236968743a
Anna <anna.93.08@mail.ru>
2022-07-11 14:59:15 +0300

    Большее из трех чисел

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if ((number1>number2) & (number1>number3))
+{
+   
+    Console.WriteLine(number1);
+}
+else
+{
+if ((number2>number3) & (number2>number3))
+{
+    Console.WriteLine(number2);
+ }
+else
+{
+    Console.WriteLine(number3);
+}
+}
