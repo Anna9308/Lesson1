848b01305fcabf7fd0e8ff9f89186e1b7e9d3bd8
Anna <anna.93.08@mail.ru>
2022-07-11 14:22:06 +0300

    Определение какое число большее, а какое меньшее

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number1);
+    Console.Write("min = ");
+    Console.Write(number2);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(number2);
+    Console.Write("min = ");
+    Console.Write(number1);
+}
