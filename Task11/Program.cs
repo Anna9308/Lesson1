eb9540029428da658fe7c0e8d48db0c9dc1ff623
Anna <anna.93.08@mail.ru>
2022-07-13 22:26:56 +0300

    Удаление второй цифры трехзначного числа

---
+++ Task11/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int digit1 = number / 100;
+int digit2 = number / 10 % 10;
+int digit3 = number % 10;
+int result = digit1*10 + digit3;
+Console.WriteLine(result);
