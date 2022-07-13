1f6bbe0fe69ce0acd1b87a11d30a4e66a128113d
Anna <anna.93.08@mail.ru>
2022-07-13 23:07:27 +0300

    Проверка является ли число квадратом другого

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+if ((N1 == N2*N2) || (N2 == N1*N1))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+
+    Console.WriteLine("Нет");
+}
