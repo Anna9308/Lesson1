089c3e9536aa7bc4a8b19c55eab3409e4f4f6dce
Anna <anna.93.08@mail.ru>
2022-07-13 23:28:09 +0300

    Вывод третьей цифры числа

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+N= Math.Abs(N);
+if (N<100)
+{
+    Console.Write("Третьей цифры нет");
+}
+//if (N>99 && N<1000)
+//{
+//    Console.Write(N % 10);
+//}
+//else
+//{
+ //   string N1 = N.ToString();
+//    Console.Write(N1[2]);
+//}
+else
+{
+    while (N>999)
+    {
+        N = N/10;
+    }
+
+Console.Write(N % 10);
+}
