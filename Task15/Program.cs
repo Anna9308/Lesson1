c4abc12159df8d0dc591102e775c267a42c5aa9c
Anna <anna.93.08@mail.ru>
2022-07-13 23:34:19 +0300

    Проверка выходного дня

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число дня недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<8)
+{
+    if (number==6 || number ==7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+}
+else{
+    Console.WriteLine("Такого дня не существует");
+}
