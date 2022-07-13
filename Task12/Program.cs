0db4a297a71ad8db4a7c8a5c083e4f018b69fd3d
Anna <anna.93.08@mail.ru>
2022-07-13 22:42:45 +0300

    Проверка кратности чисел

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+    int del = N1%N2;
+if (del==0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+
+    Console.WriteLine($"Не кратно, остаток {del}");
+}
