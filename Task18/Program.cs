e18e1ec40fe84f1f19b178c8bc4e62148b429222
Anna <anna.93.08@mail.ru>
2022-07-16 14:37:32 +0300

    Диапазон значений заданной четверти

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер четверти");
+int n = Convert.ToInt32(Console.ReadLine());
+
+switch (n)
+{
+case 1:
+Console.WriteLine("x>0, y>0");
+break;
+case 2:
+Console.WriteLine("x<0, y>0");
+break;
+case 3:
+Console.WriteLine("x<0, y<0");
+break;
+case 4:
+Console.WriteLine("x>0, y<0");
+break;
+default:
+Console.WriteLine("Вы ввели неверную четверть");
+break;
+}
