c6fc6d062804a6f6c1967d1d99085cf6e28ad524
Anna <anna.93.08@mail.ru>
2022-07-16 14:56:46 +0300

    Расстояние между двумя точками в 2Д

---
+++ Task21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите X первой точки: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y первой точки: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X второй точки: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y второй точки: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya));
+
+Console.WriteLine(d);
