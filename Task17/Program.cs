c4604240a85aff233f7149b0a14db37d6f59f2cb
Anna <anna.93.08@mail.ru>
2022-07-16 14:27:29 +0300

    Координаты четверть

---
+++ Task17/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x==0 || y==0) Console.WriteLine("x и/или y равен 0, невозможно определить четверть");
+else
+{
+    if (x>0 && y>0) Console.WriteLine("Эта точка находится в первой четверти");
+    if (x<0 && y>0) Console.WriteLine("Эта точка находится во второй четверти");
+    if (x<0 && y<0) Console.WriteLine("Эта точка находится в третьей четверти");
+    if (x>0 && y<0) Console.WriteLine("Эта точка находится в четвертой четверти");
+}
