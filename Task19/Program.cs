53f4d096306680ced2e842f452555512fac39e34
Anna <anna.93.08@mail.ru>
2022-07-21 14:39:49 +0300

    палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+string N = Convert.ToString(Console.ReadLine());
+Console.WriteLine(N);
+if ((N[0]==N[4]) && (N[1]==N[3]))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
