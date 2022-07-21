63e1ddb83de6f5b7371520bc0cfc0d83666875e5
Anna <anna.93.08@mail.ru>
2022-07-21 14:46:22 +0300

    расстояние между точками в 3D пространстве

---
+++ Task21_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите X первой точки: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y первой точки: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z первой точки: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X второй точки: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y второй точки: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z второй точки: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((xb-xa),2)+Math.Pow((yb-ya),2)+Math.Pow((zb-za),2));
+
+Console.WriteLine(d);
