e02782a2759a9de1f0266f63739ec606332ea9f2
Anna <anna.93.08@mail.ru>
2022-08-04 21:44:20 +0300

    произведение чисел от 1 до N

---
+++ Task28/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i<=n; i++)
+{
+    result = result*i;
+}
+Console.WriteLine($"Результат выполения программы = {result}");
