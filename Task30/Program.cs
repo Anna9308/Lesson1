dd1bbb0847d5e29136c855f3463a268767aeeb8e
Anna <anna.93.08@mail.ru>
2022-08-04 22:17:43 +0300

    массив из случайных элементов

---
+++ Task30/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] arr = new int[8];
+Random rand = new Random();
+int i = 0;
+while (i<arr.Length)
+{
+    arr[i] = rand.Next(0,2);
+
+    i++;
+}
+Console.WriteLine(string.Join(", ", arr));
