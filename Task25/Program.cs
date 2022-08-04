69e3a7d71de3f34ae04f3d42189c6fbc10eac102
Anna <anna.93.08@mail.ru>
2022-08-04 14:27:06 +0300

    Возведение числа А в степень В с помощью цикла for

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int pow = 1;
+
+for (int i =0; i<B; i++)
+{
+ pow = pow*A;
+}
+Console.WriteLine(pow);
