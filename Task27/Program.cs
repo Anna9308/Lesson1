d5245ea1309cc2d380e5ae87b69e93125de5feda
Anna <anna.93.08@mail.ru>
2022-08-04 22:36:53 +0300

    Cумма цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int result = 0;
+int r = 0;
+while (N>0)
+{
+    result=N%10;
+    N=N/10;
+    r = r+result;
+    i++;
+
+}
+
+Console.WriteLine($"Результат: {r}");
