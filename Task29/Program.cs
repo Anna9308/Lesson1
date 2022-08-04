b6608a44258c38dbdbf783780eee67b3e7c7ce64
Anna <anna.93.08@mail.ru>
2022-08-04 22:00:04 +0300

    ввод массива с клавиатуры и вывод на экран

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (вводим массив с клавиатуры (т. е. через консоль :) ))
+Console.WriteLine("Введите массив из 8 элементов:");
+
+int[] arr = new int[8];
+int i = 0;
+while (i<8)
+{
+    Console.Write($"Введите {i} элемент массива: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+int l=0;
+while (l<8)
+{
+    Console.WriteLine(arr[l]);
+    l++;
+}
