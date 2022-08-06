86f35ac9565704cbc48b56bb2e0088b769a1d0cd
Anna <anna.93.08@mail.ru>
2022-08-06 15:13:43 +0300

    Замена элементов положительных на отрицательные и наоборот

---
+++ Task32/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+void Change(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *=-1;
+    }
+    Console.WriteLine('[' + string.Join(", ", arr) + ']');
+}
+
+int[] array = FillArrayWithRandomNumbers (5, -9, 9);
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Change (array);
