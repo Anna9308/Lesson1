31943027691a0a47cf5ecdbef468e8755e1a3b78
Anna <anna.93.08@mail.ru>
2022-08-13 15:44:00 +0300

    Количество чисел больше 0

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел в одну линию (не используем Enter).
+//Числа разделены пробелами. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа: ");
+int[] numbers = Console.ReadLine().Split().Select(x => Convert.ToInt32(x)).ToArray();
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+PrintArray(numbers);
+
+
+int count = 0;
+for (int i=0; i < numbers.Length; i++)
+    {
+        if (numbers[i]>0)
+        {
+            count++;
+        }
+    }
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 = {count}");
