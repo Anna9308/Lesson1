38f7745b9de285ddab571b9fbb3e567584a73e9b
Anna <anna.93.08@mail.ru>
2022-08-13 14:25:45 +0300

    Вывод первых n чисел Фибоначчи без рекурсии

---
+++ Task44/Program.cs
@@ -0,0 +1,22 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+int[] fib = new int [N];
+fib[0]=0;
+fib[1]=1;
+while (i<N)
+{
+fib[i] = fib[i-1]+fib[i-2];
+i++;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+PrintArray(fib);
