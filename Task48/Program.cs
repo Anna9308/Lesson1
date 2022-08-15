3f1c462ce5dbdf95858b61eaf21414e0423f1302
Anna <anna.93.08@mail.ru>
2022-08-15 22:19:05 +0300

    Двумерный массив c элементами i+j

---
+++ Task48/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+Console.WriteLine("Задайте размерность двумерного массива mxn");
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+
+for (int i=0; i < m; i++) 
+{
+    for (int j=0; j < n; j++)
+    {
+        matrix[i,j] = i+j;
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
