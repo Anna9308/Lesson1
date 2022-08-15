1b5847c4252e53a15b5c97ce583a232410f9170e
Anna <anna.93.08@mail.ru>
2022-08-15 22:15:13 +0300

    Двумерный массив заполненный случайными целыми числами

---
+++ Task46/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+Console.WriteLine("Задайте размерность двумерного массива mxn");
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+
+for (int i=0; i < m; i++) 
+{
+    for (int j=0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(-100,100);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
