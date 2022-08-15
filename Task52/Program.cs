f3c5947a18d2157bdfdd7e673fec80e6fca0c46a
Anna <anna.93.08@mail.ru>
2022-08-15 23:35:23 +0300

    Среднее арифметическое по столбцам

---
+++ Task52/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int n = 5;
+int m = 5;
+
+int [,] matrix = new int [m,n];
+
+for (int i=0; i < m; i++) 
+{
+    for (int j=0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(1,5);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+double sum = 0;
+for (int j=0; j < n; j++) 
+{
+    for (int i=0; i < m; i++)
+    {
+        sum = sum + matrix[i,j];
+    }
+    double avg = sum/m;
+    Console.Write(avg + " ");
+    sum = 0;
+}
+
