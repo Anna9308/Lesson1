e453ad537e5cb67027a78911d40e2e3913306515
Anna <anna.93.08@mail.ru>
2022-08-15 22:40:57 +0300

    Замена элементов на квадраты с четными индексами

---
+++ Task49/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Console.WriteLine("Задайте размерность двумерного массива mxn");
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+
+for (int i=0; i < m; i++) 
+{
+    for (int j=0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(1,5);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i=0; i < m; i++) 
+{
+    for (int j=0; j < n; j++)
+    {
+        if (i%2==0 && j%2==0)
+            {
+            matrix[i,j] = matrix[i,j]*matrix[i,j];
+            }
+    Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
