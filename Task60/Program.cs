ac072375c85ed7f0a313c065af583372806d9c4d
Anna <anna.93.08@mail.ru>
2022-08-21 22:45:21 +0300

    Трехмерный массив двузначных чисел

---
+++ Task60/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int [,,] Matrix = new int [2,2,2];
+ for (int i = 0; i < Matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < Matrix.GetLength(1); j++)
+        {
+        for (int k = 0; k < Matrix.GetLength(2); k++)
+        {
+            Matrix[i, j, k] = new Random().Next(10, 99);
+             Console.WriteLine(Matrix[i, j, k] + "(" + i + "," + j + "," + k + ")");
+        }
+    }
+    }
