7ce021d0d15dc18e789d85b33ba31b65feb2ef2b
Anna <anna.93.08@mail.ru>
2022-08-21 22:27:58 +0300

    Перемножение элементов матриц

---
+++ Task58/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int[,] FillRandomMatrix(int lines = 3, int columns = 3, int leftRange = 0, int rightRange = 9)
+{
+    int[,] RandomMatrix = new int[lines, columns];
+
+    for (int i = 0; i < RandomMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < RandomMatrix.GetLength(1); j++)
+        {
+            RandomMatrix[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return RandomMatrix;
+}
+
+void PrintMatrix(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write(Array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Matrix1 = FillRandomMatrix();
+int[,] Matrix2 = FillRandomMatrix();
+PrintMatrix(Matrix1);
+Console.WriteLine();
+PrintMatrix(Matrix2);
+int[,] Matrix3 = new int[Matrix1.GetLength(0),Matrix1.GetLength(1)];
+for (int i = 0; i < Matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j < Matrix3.GetLength(1); j++)
+        {
+            Matrix3[i,j] = Matrix1[i,j]*Matrix2[i,j];
+        }
+    }
+    Console.WriteLine();
+    PrintMatrix(Matrix3);
