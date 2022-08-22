b64d46b640e6a1de7cddef66b1fe4a4036189967
Anna <anna.93.08@mail.ru>
2022-08-22 14:09:46 +0300

    Рекурсия. Все натуральные числа от m до n

---
+++ Task65/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbers(M, N);
+
+void PrintNumbers(int M1, int N1)
+{
+    if (N1<M1) return;
+    PrintNumbers(M1, N1-1);
+    
+    Console.Write(N1 + " ");
+}
